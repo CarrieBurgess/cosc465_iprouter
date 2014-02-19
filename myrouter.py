12a03ade1aef5d67a42a319e502e31c5672e1273
Carrie Burgess <cburgess@colgate.edu>
2014-02-19 00:17:26 -0500

    code complete/ works, just want to check it works on other machine too

--- myrouter.py
+++ myrouter.py
@@ -28,7 +28,24 @@ class Router(object):
                 continue
             except SrpyShutdown:
                 return
-
+            if pkt.type == pkt.ARP_TYPE:
+                arp_request = pkt.payload
+                for intf in self.net.interfaces():
+                    if (intf.ipaddr==arp_request.protodst):
+                        arp_reply = pktlib.arp()
+                        arp_reply.protodst = arp_request.protosrc
+                        arp_reply.protosrc = intf.ipaddr
+                        arp_reply.hwsrc = intf.ethaddr
+                        arp_reply.hwdst = arp_request.hwsrc
+                        arp_reply.opcode = pktlib.arp.REPLY
+                        ether = pktlib.ethernet()
+                        ether.type = ether.ARP_TYPE
+                        ether.src = intf.ethaddr
+                        ether.dst = arp_request.hwsrc
+                        ether.set_payload(arp_reply)
+                        self.net.send_packet(dev, ether)
+                        #self.net.send_packet(dev, arp_reply)
+                        break
 
 def srpy_main(net):
     '''
